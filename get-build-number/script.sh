c7237452f8c6db841c4c68dc347666d11f6c7c54
rawnly <rawnly@users.noreply.github.com>
2023-04-06 18:55:01 +0200

    fix(get-build-number): use external script + script update. Now it uses `awk` to perform math

--- get-build-number/script.sh
+++ get-build-number/script.sh
@@ -2,30 +2,25 @@
 # @see https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions
 # @see https://github.com/orgs/community/discussions/25939
 
-set -x
-
 BRANCH_NAME=${GITHUB_REF##*/}
 BRANCH_NAME=${BRANCH_NAME//\//-}
 
 if [ -z "$BRANCH_NAME" ]; then
-	echo "::error::Could not retrive BRANCH_NAME"
+	echo "::error::Could not retrive branch name"
 	exit 1
 fi
 
-echo "::notice::BRANCH_NAME=${BRANCH_NAME}"
+echo "::notice::BRANCH_NAME=$BRANCH_NAME"
 
 BUILD_NUMBER=$(git tag | sort --version-sort)
 BUILD_NUMBER=$(echo "${BUILD_NUMBER}" | grep "$BRANCH_NAME" || true)
-BUILD_NUMBER=$(echo "${BUILD_NUMBER}" | tail -n 1 | awk -F. '{print $2}')
+BUILD_NUMBER=$(echo "${BUILD_NUMBER}" | tail -n 1 | awk -F. '{n=$2+1 print n}')
 BUILD_NUMBER=$(echo "${BUILD_NUMBER}" | xargs echo -n)
 
 echo "::notice::BUILD_NUMBER=${BUILD_NUMBER}"
 
-if [ -z "$BUILD_NUMBER" ]; then
-	BUILD_NUMBER=1
-else
-	BUILD_NUMBER=$((BUILD_NUMBER + 1))
-fi
+TAG="${BRANCH_NAME}.${BUILD_NUMBER}"
 
-TAG="$BRANCH_NAME.$BUILD_NUMBER"
 echo "::notice::TAG=${TAG}"
+
+echo "$TAG"
