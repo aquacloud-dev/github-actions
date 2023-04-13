25bf1344dcb34761de101e4813a706df4c2f01d9
rawnly <rawnly@users.noreply.github.com>
2023-04-13 16:31:51 +0200

    prove

--- get-build-number/entrypoint.sh
+++ get-build-number/entrypoint.sh
@@ -5,29 +5,33 @@
 
 set -x
 
-BRANCH_NAME=${GITHUB_REF##*/}
-BRANCH_NAME=$(echo "$BRANCH_NAME" | sed 's/\//-/g')
+git fetch --tags
 
-if [ -z "$BRANCH_NAME" ]; then
-	echo "::error::Could not retrive branch name"
-	exit 1
-fi
+echo "$(git tag)"
 
-echo "::notice::BRANCH_NAME=$BRANCH_NAME"
+# BRANCH_NAME=${GITHUB_REF##*/}
+# BRANCH_NAME=$(echo "$BRANCH_NAME" | sed 's/\//-/g')
+#
+# if [ -z "$BRANCH_NAME" ]; then
+# 	echo "::error::Could not retrive branch name"
+# 	exit 1
+# fi
+#
+# echo "::notice::BRANCH_NAME=$BRANCH_NAME"
+#
+# BUILD_NUMBER=$(git tag | sort --version-sort)
+# echo "::debug::BUILD_NUMBER=${BUILD_NUMBER}"
+# BUILD_NUMBER=$(echo "${BUILD_NUMBER}" | grep "$BRANCH_NAME" || true)
+# echo "::debug::BUILD_NUMBER=${BUILD_NUMBER}"
+# BUILD_NUMBER=$(echo "${BUILD_NUMBER}" | tail -n 1 | awk -F. '{n=$2+1 print n}')
+# echo "::debug::BUILD_NUMBER=${BUILD_NUMBER}"
+# BUILD_NUMBER=$(echo "${BUILD_NUMBER}" | xargs echo -n)
+# echo "::debug::BUILD_NUMBER=${BUILD_NUMBER}"
+#
+# echo "::notice::BUILD_NUMBER=${BUILD_NUMBER}"
+#
+# TAG="${BRANCH_NAME}.${BUILD_NUMBER}"
+#
+# echo "::notice::TAG=${TAG}"
 
-BUILD_NUMBER=$(git tag | sort --version-sort)
-echo "::debug::BUILD_NUMBER=${BUILD_NUMBER}"
-BUILD_NUMBER=$(echo "${BUILD_NUMBER}" | grep "$BRANCH_NAME" || true)
-echo "::debug::BUILD_NUMBER=${BUILD_NUMBER}"
-BUILD_NUMBER=$(echo "${BUILD_NUMBER}" | tail -n 1 | awk -F. '{n=$2+1 print n}')
-echo "::debug::BUILD_NUMBER=${BUILD_NUMBER}"
-BUILD_NUMBER=$(echo "${BUILD_NUMBER}" | xargs echo -n)
-echo "::debug::BUILD_NUMBER=${BUILD_NUMBER}"
-
-echo "::notice::BUILD_NUMBER=${BUILD_NUMBER}"
-
-TAG="${BRANCH_NAME}.${BUILD_NUMBER}"
-
-echo "::notice::TAG=${TAG}"
-
-echo "tag=$TAG" >>$GITHUB_OUTPUT
+# echo "tag=$TAG" >>$GITHUB_OUTPUT
